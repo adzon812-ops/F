fba3b101333a70639abab8109035796d6bd3324e
adzon812-ops <adzon812@gmail.com>
2026-03-05 10:18:15 +0500

    Update providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,10 +1,21 @@
 'use client'
 
 import { ThemeProvider } from 'next-themes'
+import { useEffect, useState } from 'react'
 
 export function Providers({ children }: { children: React.ReactNode }) {
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) {
+    return <div className="min-h-screen bg-gray-50" />
+  }
+
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
       {children}
     </ThemeProvider>
   )
